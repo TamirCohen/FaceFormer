cb75f719b2339281944230a3962b58244178015e
moti.rizel <moti.rizel@gmail.com>
2023-08-31 15:50:18 +0300

    remove breakpoint

--- main.py
+++ main.py
@@ -83,7 +83,6 @@ def test(args, model, test_loader,epoch):
 
     print ("Load the model...")
     print(os.path.join(save_path, '{}_model.pth'.format(epoch)))
-    breakpoint()
     model.load_state_dict(torch.load(os.path.join(save_path, '{}_model.pth'.format(epoch))))
     model = model.to(torch.device("cuda"))
     model.eval()
