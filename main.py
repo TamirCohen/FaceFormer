874208d869792bb421617162c8a33dd8ff6fd4a6
moti.rizel <moti.rizel@gmail.com>
2023-09-03 20:39:31 +0300

    change loss calc to creterion func

--- main.py
+++ main.py
@@ -109,11 +109,11 @@ def test(args, model, test_loader,epoch, criterion):
             prediction = model.predict(audio, template, one_hot)
 
             #breakpoint()
-            #loss = criterion(prediction, vertice[:,1:128,:])
+            loss = criterion(prediction, vertice[:,1:prediction.shape[1]+1,:])
             #print("shapes:", prediction.shape, vertice.shape)
-            sum = torch.sum((prediction - vertice[:,1:prediction.shape[1]+1,:])**2)
-            sqrt = torch.sqrt(sum)
-            loss = torch.mean(sqrt)
+            #sum = torch.sum((prediction - vertice[:,1:prediction.shape[1]+1,:])**2)
+            #sqrt = torch.sqrt(sum)
+            #loss = torch.mean(sqrt)
             #loss = torch.mean(torch.sqrt(torch.sum((prediction - vertice[:,1:128,:])**2, dim = -1)))
             test_loss_log.append(loss.item())
 
@@ -127,11 +127,11 @@ def test(args, model, test_loader,epoch, criterion):
                 prediction = model.predict(audio, template, one_hot)
 
                 #breakpoint()
-                #loss = criterion(prediction, vertice[:,1:128,:])
+                loss = criterion(prediction, vertice[:,1:prediction.shape[1]+1,:])
                 #print("shapes:", prediction.shape, vertice.shape)
-                sum = torch.sum((prediction - vertice[:,1:prediction.shape[1]+1,:])**2)
-                sqrt = torch.sqrt(sum)
-                loss = torch.mean(sqrt)
+                #sum = torch.sum((prediction - vertice[:,1:prediction.shape[1]+1,:])**2)
+                #sqrt = torch.sqrt(sum)
+                #loss = torch.mean(sqrt)
                 #loss = torch.mean(torch.sqrt(torch.sum((prediction - vertice[:,1:128,:])**2, dim=-1)))
                 test_loss_log.append(loss.item())
 
